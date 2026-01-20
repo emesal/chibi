84441832624b8d7d71285d6d8f371e3d752727b0
fey <fey@emesal.net>
2026-01-20 08:24:24 +0000

    let's see if this works

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Chibi - A CLI tool for conversing with LLMs via OpenRouter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "chibi";
+          version = "0.2.0";
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+          ];
+
+          buildInputs = [
+            pkgs.openssl
+          ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.darwin.apple_sdk.frameworks.Security
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+
+          meta = {
+            description = "CLI tool for conversing with LLMs via OpenRouter";
+            homepage = "https://github.com/emesal/chibi";
+            license = pkgs.lib.licenses.mit;
+            maintainers = [ ];
+            mainProgram = "chibi";
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = [
+            pkgs.cargo
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.clippy
+          ];
+        };
+      }
+    );
+}
