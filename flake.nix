9cdb2f57c61e4869da8957e41f4c62861b3c8ae4
fey <fey@emesal.net>
2026-01-26 23:16:01 +0000

    it's 0.5.0-dev now

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "chibi";
-          version = "0.5.0";
+          version = "0.5.0-dev";
           src = ./.;
 
           cargoLock = {
