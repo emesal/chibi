bc0fdde3db0c253bbf90a2003740d90fe47c5d43
fey <fey@emesal.net>
2026-02-13 08:19:02 +0000

    cargo fmt

--- crates/chibi-cli/build.rs
+++ crates/chibi-cli/build.rs
@@ -1,10 +1,7 @@
 use vergen_gitcl::{Emitter, GitclBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let gitcl = GitclBuilder::default()
-        .branch(true)
-        .sha(true)
-        .build()?;
+    let gitcl = GitclBuilder::default().branch(true).sha(true).build()?;
 
     Emitter::default().add_instructions(&gitcl)?.emit()?;
 
